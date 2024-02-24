3422b52729b3e5ec10d9c252605b32495087f7c3
hydroft1 <marquetalexandre@proton.me>
2024-02-24 11:17:19 +0100

    commit

--- components/Header.js
+++ components/Header.js
@@ -1,14 +1,15 @@
-import { StyleSheet, Text, View } from "react-native";
+import { StyleSheet, Text, View, Image } from "react-native";
 import React from "react";
 
 const Header = (props) => {
   return (
-    <View style={{ marginLeft: 15 }}>
-      <Text style={{ fontWeight: "bold", fontSize: 28 }}>{props.name}</Text>
+    <View style={{ marginLeft: 15, alignItems: "center" }}>
+      <Image source={require("../assets/banner.png")} />
+      <Text style={{ fontWeight: "bold", fontSize: 28}}>{props.name}</Text>
     </View>
   );
 };
 
 export default Header;
 
-const styles = StyleSheet.create({});
+
