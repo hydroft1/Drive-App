c5aaf0d17564d769659208151665e5d7bbb4c355
hydroft1 <marquetalexandre@proton.me>
2024-02-24 10:08:27 +0100

    création du fichier config pour firebase

--- App.js
+++ App.js
@@ -3,16 +3,25 @@ import { StyleSheet, Text, View } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
 import { HomeScreen } from './screens';
+import React, { useState, useEffect } from 'react';
+import { firebase } from './config';
+
+import LoginScreen from './screens';
+import SignUpScreen from './screens';
+import HomeScreen from './screens';
+import Header from './components/Header';
 
 const Stack = createNativeStackNavigator();
 
 export default function App() {
+  const [initializing, setInitializing] = useState(true);
   return (
     <NavigationContainer>
       <Stack.Navigator screenOptions={{ headerShown: false }}>
         <Stack.Screen name="HomeScreen" component={HomeScreen} />
       </Stack.Navigator>
     </NavigationContainer>
+    
   );
 }
 
---
+++ config.js
@@ -0,0 +1,20 @@
+// firebase config key setup
+
+import firebase from "firebase/compat/app";
+import "firebase/compat/auth";
+
+// firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyA21rVqxj13wAmHBASmqaVhIAZGl4km314",
+  authDomain: "drive-aac.firebaseapp.com",
+  projectId: "drive-aac",
+  storageBucket: "drive-aac.appspot.com",
+  messagingSenderId: "680037822919",
+  appId: "1:680037822919:web:e1fd16033f8841b945e9e2",
+};
+
+if (!firebase.apps.length) {
+  firebase.initializeApp(firebaseConfig);
+}
+
+export { firebase };
